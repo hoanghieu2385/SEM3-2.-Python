a948254e37df1ef839f254baec1721568ab924fc
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-09-25 09:11:02 +0700

    update bài tập buổi 5

--- B5/student_subject.py
+++ B5/student_subject.py
@@ -1,4 +1,3 @@
-# Clear code previous
 import os
 os.system("cls")
 from tabulate import tabulate
@@ -12,8 +11,8 @@ def connect_to_db():
         password = "",
         database = "sem3_py"
     )
-    
-def get_info():
+
+def get_all_student():
     connection = connect_to_db()
     cursor = connection.cursor()
     
@@ -26,7 +25,30 @@ def get_info():
     headers = ["ID", "Name", "Age", "Class"]
     print(tabulate(students, headers=headers))
 
-def add_student_subject(id_student, id_subject):
+def get_info_student_subject():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+
+    # Select all records from the students_subjects table
+    select_query = """
+    SELECT students.id, students.Name, subjects.name
+    FROM students
+    INNER JOIN students_subjects ON students.id = students_subjects.id_student
+    INNER JOIN subjects ON subjects.id = students_subjects.id_subject
+    """
+    cursor.execute(select_query)
+
+    # Fetch all the rows and print them out in a table format
+    records = cursor.fetchall()
+    print("Student-Subject relationship:")
+    headers = ["Student ID", "Student Name", "Subject Name"]
+    print(tabulate(records, headers=headers))
+
+def add_student_subject():
+    get_all_student()
+
+    id_student = input("Enter student ID: ")
+    id_subject = input("Enter subject ID: ")
     connection = connect_to_db()
     cursor = connection.cursor()
     query = "INSERT INTO students_subjects (id_student, id_subject) VALUES (%s, %s)"
@@ -35,16 +57,42 @@ def add_student_subject(id_student, id_subject):
     connection.commit()
     print(cursor.rowcount, "record inserted.")
 
-def get_students_by_subject(id_subject):
+def get_students_by_subject_id():
+    get_info_student_subject()
+
+    subject_name = input("Enter subject id: ")
     connection = connect_to_db()
     cursor = connection.cursor()
-    query = "SELECT * FROM students WHERE id IN (SELECT id_student FROM students_subjects WHERE id_subject = %s)"
-    values = (id_subject,)
-    cursor.execute(query, values)
-    result = cursor.fetchall()
-    for x in result:
-        print(x)
-        
+
+    # Select all students who are enrolled in the given subject
+    select_query = """
+    SELECT students.id, students.Name, students.Age, students.Class
+    FROM students
+    INNER JOIN students_subjects ON students.id = students_subjects.id_student
+    INNER JOIN subjects ON subjects.id = students_subjects.id_subject
+    WHERE subjects.id = %s
+    """
+    values = (subject_name,)
+    cursor.execute(select_query, values)
+
+    # Fetch all the rows and print them out in a table format
+    students = cursor.fetchall()
+    print(f"Students enrolled in {subject_name}:")
+    headers = ["ID", "Name", "Age", "Class"]
+    print(tabulate(students, headers=headers))
+
+def menu():
+    while True:    
+        print("\n=== MENU ===")
+        print("1. Add student and subject")
+        print("2. Get student by subject id")
+
+        choice = input("Enter number your choice: ")
+        if choice == "1":
+            add_student_subject()
+        elif choice == "2":
+            get_students_by_subject_id()
+        else:
+            print("\nPlease enter a valid choice !!!")
 if __name__ == '__main__':
-    # add_student_subject(3, "hoahoc")
-    get_students_by_subject("hoahoc")
+    menu()
--- B5/subject.py
+++ B5/subject.py
@@ -13,16 +13,95 @@ def connect_to_db():
         database = "sem3_py"
     )
     
-def get_info():
+def get_subjects():
     connection = connect_to_db()
     cursor = connection.cursor()
     
     # Select all students from the database
-    select_query = "SELECT `id`, `Name`, `Age`, `Class` FROM students"
+    select_query = "SELECT `id`, `name` FROM subjects"
     cursor.execute(select_query)
     # Fetch all the rows and print them out in a table format
     students = cursor.fetchall()
-    print("Updated students:")
-    headers = ["ID", "Name", "Age", "Class"]
+    print("\n=== Subjects ===:")
+    headers = ["ID", "Name"]
     print(tabulate(students, headers=headers))
+    
+def add_subject():
+    get_subjects()
+    
+    try:
+        id = input("Enter subject ID: ")
+        name = input("Enter student name: ")
+        connection = connect_to_db()
+        cursor = connection.cursor()
+        
+        insert_query = "INSERT INTO students (id, name) VALUES (%s, %s)"
+        cursor.execute(insert_query, (id, name))
+        
+        connection.commit()
+        print(f"Add subject: {id} with {name} successful!")
+        
+        connection.close()
+    except :
+        connection.rollback()
+        print(f"Add subject FAIL !")
+        
+    get_subjects()
+    
+    connection.close()
 
+def edit_subject():
+    get_subjects()
+    
+    new_subject_id = int(input("Enter subject id: "))
+
+    new_name = input("Enter NEW subject name: ")
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    
+    update_query = "UPDATE `subjects` SET `name`= %s WHERE id = %s"
+    cursor.execute(update_query, (new_subject_id, new_name))
+    
+    connection.commit()
+    print(f"Student with id: {new_subject_id} has been updated!!!")
+    
+    connection.close()
+
+def delete_subject():
+    get_subjects()
+
+    new_subject_id = int(input("Enter student id: "))
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+
+    delete_query = "DELETE FROM subjects WHERE id = %s"
+    # cursor.execute() yêu cầu tham số phải là một tuple, ngay cả khi chỉ có một phần tử.
+    # Trong trường hợp chỉ có một giá trị, bạn cần thêm dấu phẩy sau giá trị để biến nó thành tuple
+    cursor.execute(delete_query, (new_subject_id,))
+    
+    connection.commit()
+    print(f"Student with id: {new_subject_id} has been deleted!!!")
+
+    get_subjects()
+    
+    connection.close()
+
+def menu():
+    while True:    
+        print("\n=== MENU ===")
+        print("1. Add student and subject")
+        print("2. Get student by subject id")
+
+        choice = input("Enter number your choice: ")
+        if choice == "1":
+            add_subject()
+        elif choice == "2":
+            edit_subject()
+        elif choice == "3":
+            delete_subject()
+        else:
+            print("\nPlease enter a valid choice !!!")
+if __name__ == '__main__':
+    menu()
